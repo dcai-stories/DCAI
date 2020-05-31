0c9cdfb33328f07f161f164526c524afdfde55da
dcai-stories <66195418+dcai-stories@users.noreply.github.com>
2020-05-31 00:10:41 -0700

    Create utils.py

---
+++ weak_supervision/utils.py
@@ -0,0 +1,22 @@
+import json
+from typing import List
+
+def write_items(items: List[str], output_file):
+    with open(output_file, 'w') as f:
+        for item in items:
+            f.write(str(item) + "\n")
+    f.close()
+
+
+def read_lines(input_file: str) -> List[str]:
+    lines = []
+    with open(input_file, "rb") as f:
+        for l in f:
+            lines.append(l.decode().strip())
+    return lines
+
+
+def read_jsonl_lines(input_file: str) -> List[dict]:
+    with open(input_file) as f:
+        lines = f.readlines()
+        return [json.loads(l.strip()) for l in lines]
